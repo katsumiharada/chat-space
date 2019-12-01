d26aaa91ce61f4cdadad16859c228da4fe4e1959
katsumiharada <katsumi0716@i.softbank.jp>
2019-12-01 18:54:24 +0900

    initial commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     var image = message.image ? `<img class="lower-message__image" src="${message.image}">`: ``;
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${message.user_name}
@@ -36,15 +36,37 @@ $(function(){
       contentType: false,
     })
     .done(function(message){
-      console.log(message)
       var html = buildHTML(message);
       $('.messages').append(html);
-      $(".form__submit").prop('disabled', false);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
     })
     .fail(function(){
       alert('メッセージ送信に失敗しました');
-    });
+    })
+    return false;
   });
-})
+    var reloadMessages = function() {
+      if (location.href.match(/\/groups\/\d+\/message/)){
+        last_message_id = $(".message:last").data("message-id");
+        $.ajax({
+          url: "api/messages",
+          type: 'get',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
+        .done(function(messages) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        })
+        .fail(function() {
+          alert('自動更新に失敗しました');
+        })
+      }
+    }
+    setInterval(reloadMessages, 7000);
+});
